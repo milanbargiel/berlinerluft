59ac8f23b432a6a77d9aaf72fb51f8e0cdcd6dbc
milanbargiel <milan.bargiel@googlemail.com>
2021-01-13 16:53:09 +0100

    Make d3 code eslint compliant

--- visualization/dev/js/main.js
+++ visualization/dev/js/main.js
@@ -1,84 +1,83 @@
-function drawCalendar(dateData){
-  console.log(dateData);
-  
-  var weeksInMonth = function(month){
-    var m = d3.timeMonth.floor(month)
-    return d3.timeWeeks(d3.timeWeek.floor(m), d3.timeMonth.offset(m,1)).length;
-  }
+function drawCalendar(dateData) {
+  const weeksInMonth = (month) => {
+    const m = d3.timeMonth.floor(month);
+    return d3.timeWeeks(d3.timeWeek.floor(m), d3.timeMonth.offset(m, 1)).length;
+  };
 
-  var minDate = d3.min(dateData, function(d) { return new Date(d.day) })
-  var maxDate = d3.max(dateData, function(d) { return new Date(d.day) })
+  const minDate = d3.min(dateData, (d) => new Date(d.day));
+  const maxDate = d3.max(dateData, (d) => new Date(d.day));
 
-  var cellMargin = 2,
-      cellSize = 20;
+  const cellMargin = 2;
+  const cellSize = 20;
 
-  var day = d3.timeFormat("%w"),
-      week = d3.timeFormat("%U"),
-      format = d3.timeFormat("%Y-%m-%d"),
-      titleFormat = d3.utcFormat("%a, %d-%b");
-      monthName = d3.timeFormat("%B"),
-      months= d3.timeMonth.range(d3.timeMonth.floor(minDate), maxDate);
+  const day = d3.timeFormat('%w');
+  const week = d3.timeFormat('%U');
+  const format = d3.timeFormat('%Y-%m-%d');
+  const titleFormat = d3.utcFormat('%a, %d-%b');
+  const monthName = d3.timeFormat('%B');
+  const months = d3.timeMonth.range(d3.timeMonth.floor(minDate), maxDate);
 
-  var svg = d3.select("#calendar").selectAll("svg")
+  const svg = d3.select('#calendar').selectAll('svg')
     .data(months)
-    .enter().append("svg")
-    .attr("class", "month")
-    .attr("height", ((cellSize * 7) + (cellMargin * 8) + 20) ) // the 20 is for the month labels
-    .attr("width", function(d) {
-      var columns = weeksInMonth(d);
+    .enter()
+    .append('svg')
+    .attr('class', 'month')
+    .attr('height', ((cellSize * 7) + (cellMargin * 8) + 20)) // the 20 is for the month labels
+    .attr('width', (d) => {
+      const columns = weeksInMonth(d);
       return ((cellSize * columns) + (cellMargin * (columns + 1)));
     })
-    .append("g")
+    .append('g');
 
-  svg.append("text")
-    .attr("class", "month-name")
-    .attr("y", (cellSize * 7) + (cellMargin * 8) + 15 )
-    .attr("x", function(d) {
-      var columns = weeksInMonth(d);
+  svg.append('text')
+    .attr('class', 'month-name')
+    .attr('y', (cellSize * 7) + (cellMargin * 8) + 15)
+    .attr('x', (d) => {
+      const columns = weeksInMonth(d);
       return (((cellSize * columns) + (cellMargin * (columns + 1))) / 2);
     })
-    .attr("text-anchor", "middle")
-    .text(function(d) { return monthName(d); })
+    .attr('text-anchor', 'middle')
+    .text((d) => monthName(d));
 
-  var rect = svg.selectAll("rect.day")
-    .data(function(d, i) { return d3.timeDays(d, new Date(d.getFullYear(), d.getMonth()+1, 1)); })
-    .enter().append("rect")
-    .attr("class", "day")
-    .attr("width", cellSize)
-    .attr("height", cellSize)
-    .attr("rx", 3).attr("ry", 3) // rounded corners
-    .attr("fill", '#eaeaea') // default light grey fill
-    .attr("y", function(d) { return (day(d) * cellSize) + (day(d) * cellMargin) + cellMargin; })
-    .attr("x", function(d) { return ((week(d) - week(new Date(d.getFullYear(),d.getMonth(),1))) * cellSize) + ((week(d) - week(new Date(d.getFullYear(),d.getMonth(),1))) * cellMargin) + cellMargin ; })
-    .on("mouseover", function(d) {
+  const rect = svg.selectAll('rect.day')
+    .data((d) => d3.timeDays(d, new Date(d.getFullYear(), d.getMonth() + 1, 1)))
+    .enter().append('rect')
+    .attr('class', 'day')
+    .attr('width', cellSize)
+    .attr('height', cellSize)
+    .attr('rx', 3)
+    .attr('ry', 3) // rounded corners
+    .attr('fill', '#eaeaea') // default light grey fill
+    .attr('y', (d) => (day(d) * cellSize) + (day(d) * cellMargin) + cellMargin)
+    .attr('x', (d) => ((week(d) - week(new Date(d.getFullYear(), d.getMonth(), 1))) * cellSize) + ((week(d) - week(new Date(d.getFullYear(), d.getMonth(), 1))) * cellMargin) + cellMargin)
+    .on('mouseover', function () {
       d3.select(this).classed('hover', true);
     })
-    .on("mouseout", function(d) {
+    .on('mouseout', function () {
       d3.select(this).classed('hover', false);
     })
     .datum(format);
 
-  rect.append("title")
-    .text(function(d) { return titleFormat(new Date(d)); });
+  rect.append('title')
+    .text((d) => titleFormat(new Date(d)));
 
-  var lookup = d3.nest()
-    .key(function(d) { return d.day; })
-    .rollup(function(leaves) {
-      return d3.sum(leaves, function(d){ return parseInt(d.count); });
-    })
+  const lookup = d3.nest()
+    .key((d) => d.day)
+    // eslint-disable-next-line radix
+    .rollup((leaves) => d3.sum(leaves, (d) => parseInt(d.count)))
     .object(dateData);
 
-  var scale = d3.scaleLinear()
-    .domain(d3.extent(dateData, function(d) { return parseInt(d.count); }))
-    .range([0.4,1]); // the interpolate used for color expects a number in the range [0,1] but i don't want the lightest part of the color scheme
+  const scale = d3.scaleLinear()
+    .domain(d3.extent(dateData, (d) => parseInt(d.count)))
+    .range([0.4, 1]); // the interpolate used for color expects a number in the range
+    // [0,1] but i don't want the lightest part of the color scheme
 
-  rect.filter(function(d) { return d in lookup; })
-    .style("fill", function(d) { return d3.interpolatePuBu(scale(lookup[d])); })
-    .select("title")
-    .text(function(d) { return titleFormat(new Date(d)) + ":  " + lookup[d]; });
-
+  rect.filter((d) => d in lookup)
+    .style('fill', (d) => d3.interpolatePuBu(scale(lookup[d])))
+    .select('title')
+    .text((d) => titleFormat(`${new Date(d)}: ${lookup[d]}`));
 }
 
-d3.csv("assets/data.csv", function(response){
+d3.csv('assets/data.csv', (response) => {
   drawCalendar(response);
-})
+});
